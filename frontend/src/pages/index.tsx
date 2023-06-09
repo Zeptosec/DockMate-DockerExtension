b90c8523545dabcc7d636034b133c83f5d930f9a
Sauliusm1 <sauzen@ktu.lt>
2023-06-09 21:07:40 +0300

    Input Ui

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -7,7 +7,34 @@ export default function Home() {
   
   return (
     <main>
+        <div className='break'>
+      <h3>Dockmate extension demo</h3>
+      </div>
+      <div className='break'>
+      <button className='button1'>
+      Get data
+      </button>
+      </div>
+      <div className='break'>
+      <form>
+        <div><label htmlFor="Service">Service</label></div>
+        <input type='text'>
+      
+      </input>
+      <div className='break'>
+      <div><label htmlFor="MaxSize">Max Service Size(GB)</label></div>
+      <input type='number'>
+      
+      </input>
+      </div>
+      <button className='button2'>
+      Create Service
+      </button>
+      </form>
+      </div>
+    
      const: {containers}
+
     </main>
   )
 }
