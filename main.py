14b37a8b773b6cd4f48bb9c6317a5f91981a0aff
Zeptosec <armandasmatkevicius@gmail.com>
2023-06-09 19:28:12 +0300

    fixed styles

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 from swarm import Swarm
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+mySwarm = Swarm()
+print(mySwarm)
+exit(1)
 app = FastAPI()
 
 class Item(BaseModel):
     name: str
     requiredMemory: int
 
-items = {
-    0: Item(name="Nice")
-}
+
 
 @app.get("/")
 def index() -> dict[str, dict[int, Item]]:
+    items = {
+        
+    }
     return {"items": items}
 
-# mySwarm = Swarm()
+
 # print("Before BF algorithm:")
 # for container in mySwarm.containers:
 #     print(f"node: {container.id}, freeMem: {container.free_memory}")
